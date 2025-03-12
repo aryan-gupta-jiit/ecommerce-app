ae41c5f8514f98fabae91461dbffcd3ac2a55600
aryan-gupta-jiit <aryangupta.jiit@gmail.com>
2025-03-12 20:36:25 +0530

    home page completed

--- frontend/src/components/NewsletterBox.jsx
+++ frontend/src/components/NewsletterBox.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
 const NewsletterBox = () => {
+
+    const onSubmitHandler=(event)=>{
+        event.preventDefault();
+    }
+
   return (
-    <div>NewsletterBox</div>
+    <div className='text-center'>
+        <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off</p>
+        <p className='text-gray-400 mt-3'>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci, quia sint quo perspiciatis numquam dolor odit.
+        </p>
+        <form onSubmit={onSubmitHandler} className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3'>
+            <input type="email" placeholder='Enter your email' className='w-full sm:flex-1 outline-none' required/>
+            <button type='submit' className='bg-black text-white text-xs px-10 py-4'></button>
+        </form>
+    </div>
   )
 }
 
