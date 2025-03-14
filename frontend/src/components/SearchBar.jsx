cae99da61bb00d51a49a29cf7a8c127d18674877
aryan-gupta-jiit <aryangupta.jiit@gmail.com>
2025-03-14 23:07:09 +0530

    cartpage

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -5,7 +5,7 @@ import { useLocation } from 'react-router-dom'
 
 const SearchBar = () => {
     const {search,setSearch,showSearch,setShowSearch}=useContext(ShopContext)
-    const [visible,setVisible]=useState(false)
+    const [visible,setVisible]=useState(showSearch)
     const location=useLocation();
 
     useEffect(()=>{
@@ -15,12 +15,12 @@ const SearchBar = () => {
         else{
             setVisible(false)
         }
-    },[location])
+    },[location,showSearch])
 
     return showSearch && visible ? (
     <div className='border-t border-b bg-gray-50 text-center'>
         <div className='inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2'>
-            <input type="text" placeholder='Search' className='flex-1 outline-none bg-inherit text-sm' value={search} onChange={()=>setSearch(e.target.value)}/>
+            <input type="text" placeholder='Search' className='flex-1 outline-none bg-inherit text-sm' value={search} onChange={(e)=>setSearch(e.target.value)}/>
             <img src={assets.search_icon} alt="" className='w-4'/>
         </div>
         <img onClick={()=>setShowSearch(false)} src={assets.cross_icon} alt="" className='inline w-3 cursor-pointer'/>
