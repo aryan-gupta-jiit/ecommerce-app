62900d928464c8b8a8cd850dab13ae143c53f806
aryan-gupta-jiit <aryangupta.jiit@gmail.com>
2025-04-12 19:23:55 +0530

    backend models initialisation

---
+++ backend/controllers/userController.js
@@ -0,0 +1,20 @@
+
+
+// route for user login
+const loginUser=async(req,res)=>{
+
+}
+
+// route for user registration
+
+const registerUser=async(req,res)=>{
+    res.json({msg:"register API working"})
+}
+
+// route for admin login
+
+const adminLogin=async(req,res)=>{
+
+}
+
+export {loginUser,registerUser,adminLogin};
