2d8a97759236f8abfe41361c48f0a3da65a35552
aryan-gupta-jiit <aryangupta.jiit@gmail.com>
2025-04-20 13:31:39 +0530

    connecting admin with backend

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
 import Add from './pages/Add.jsx'
@@ -7,22 +7,31 @@ import List from './pages/List.jsx'
 import Orders from './pages/Orders.jsx'
 import { Routes,Route } from 'react-router-dom'
 import Login from './components/Login.jsx'
+import { ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+
+export const backendUrl=import.meta.env.VITE_BACKEND_URL
+export const currency='$'
 
 const App = () => {
-  const [token,setToken]=useState('')
+  const [token,setToken]=useState(localStorage.getItem('token')?localStorage.getItem('token'):'')
+  useEffect(()=>{
+    localStorage.setItem('token',token)
+  })
   return (
     <div className='bg-gray-50 min-h-screen'>
+      <ToastContainer/>
       {token==='' 
-      ? <Login/>
+      ? <Login setToken={setToken}/>
     : <>
-    <Navbar/>
+    <Navbar setToken={setToken}/>
     <hr />
     <div className='flex w-full'>
       <Sidebar/>
       <div className='w-[70%] mx-auto ml-[max(5vw, 25px)] my-8  text-gray-600 text-base'>
         <Routes>
-          <Route path="/add" element={<Add />} />
-          <Route path="/list" element={<List />} />
+          <Route path="/add" element={<Add token={token}/>} />
+          <Route path="/list" element={<List token={token}/>} />
           <Route path="/orders/" element={<Orders />} />
         </Routes>
       </div>
